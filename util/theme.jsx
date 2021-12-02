9b2c73eae136288f40198a5452740825ece42825
valentinoguerra <50251316+valentinoguerra@users.noreply.github.com>
2021-12-01 21:33:54 -0300

    colors 2

--- util/theme.jsx
+++ util/theme.jsx
@@ -1,17 +1,17 @@
 import Color from "color";
 
 // Colors
-const primary = Color("#1b5dc0");
+const primary = Color("#173f7a");
 const accent = Color("#eb6c23");
-const neutral = Color("#949191");
+const neutral = Color("#929292");
 
 export const theme = {
   colors: {
     // Primary
-    primary100: primary.lighten(0.8).hex(),
-    primary200: primary.lighten(0.6).hex(),
-    primary300: primary.lighten(0.4).hex(),
-    primary400: primary.lighten(0.2).hex(),
+    primary100: primary.lighten(1.5).hex(),
+    primary200: primary.lighten(1.2).hex(),
+    primary300: primary.lighten(0.9).hex(),
+    primary400: primary.lighten(0.3).hex(),
     primary500: primary.lighten(0).hex(),
     primary600: primary.lighten(-0.2).hex(),
     primary700: primary.lighten(-0.4).hex(),
@@ -19,8 +19,8 @@ export const theme = {
     primary900: primary.lighten(-0.8).hex(),
     // Accent
     accent100: accent.lighten(0.8).hex(),
-    accent200: accent.lighten(0.6).hex(),
-    accent300: accent.lighten(0.4).hex(),
+    accent200: accent.lighten(0.7).hex(),
+    accent300: accent.lighten(0.5).hex(),
     accent400: accent.lighten(0.2).hex(),
     accent500: accent.lighten(0).hex(),
     accent600: accent.lighten(-0.2).hex(),
@@ -28,10 +28,10 @@ export const theme = {
     accent800: accent.lighten(-0.6).hex(),
     accent900: accent.lighten(-0.8).hex(),
     // Neutral
-    neutral100: neutral.lighten(0.8).hex(),
+    neutral100: neutral.lighten(0.65).hex(),
     neutral200: neutral.lighten(0.6).hex(),
     neutral300: neutral.lighten(0.4).hex(),
-    neutral400: neutral.lighten(0.2).hex(),
+    neutral400: neutral.lighten(0.25).hex(),
     neutral500: neutral.lighten(0).hex(),
     neutral600: neutral.lighten(-0.2).hex(),
     neutral700: neutral.lighten(-0.4).hex(),
