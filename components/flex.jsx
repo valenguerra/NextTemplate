b5411bef30269e529a3944feaec83b6688a59497
valentinoguerra <50251316+valentinoguerra@users.noreply.github.com>
2022-03-08 18:34:36 -0300

    Update flex, column and row

--- components/flex.jsx
+++ components/flex.jsx
@@ -2,20 +2,18 @@ import styled from "styled-components";
 import { layout, position, space } from "styled-system";
 import { cssFlexbox } from "../util/custom-properties";
 
-export const Column = styled.div`
+export const Flexbox = styled.div`
   display: flex;
-  flex-direction: column;
   ${cssFlexbox}
   ${position}
   ${layout}
   ${space}
 `;
 
-export const Row = styled.div`
-  display: flex;
+export const Column = styled(Flexbox)`
+  flex-direction: column;
+`;
+
+export const Row = styled(Flexbox)`
   flex-direction: row;
-  ${cssFlexbox}
-  ${position}
-  ${layout}
-  ${space}
 `;
